8d000a1cca94211ebfd1000e1d9cf65d69c3ee68
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-12-29 22:03:43 -0600

    added route for login and authenticated routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Layout from './components/layout/Layout'
-import Home from './pages/Home'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import Home from "./pages/Home";
+import LoginPage from "./pages/Login";
 
 function App() {
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route
+          path="/"
+          element={
+            <AuthenticatedRoute>
+              <Layout />
+            </AuthenticatedRoute>
+          }
+        >
           <Route index element={<Home />} />
         </Route>
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
