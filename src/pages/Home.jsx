4cdefe443bfea8bbc1e2552b9c951298820b8ffa
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-12-29 22:26:42 -0600

    added home-page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
-export default function Home() {
-    return (
-        <div>
-            <h1 className="title">Welcome to Enhanced Beauty</h1>
-        </div>
-    )
+function Home() {
+  return (
+    <div>
+      <h1 className="title">Welcome to Enhanced Beauty</h1>
+    </div>
+  );
 }
+
+export default Home;
